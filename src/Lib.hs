05428beafcc8010db6634b756b0b00c34d8976fa
Louis Monestier <louis.monestier@contractor.akur8.com>
2023-12-04 11:57:50 +0100

    day 4

--- src/Lib.hs
+++ src/Lib.hs
@@ -15,23 +15,45 @@ module Lib
     parseOrError,
     padZeros,
     Parser,
-    sourceColI
+    sourceColI,
+    pInt,
+    pKeyword,
   )
 where
-  
-import Data.List as L
-import Data.Function
-import Data.Maybe
-import Text.Printf
-import qualified Data.List.Split as Split
 
-import Text.Megaparsec hiding(State)
+import Data.Function
+import Data.List as L
+import qualified Data.List.Split as Split
+import Data.Maybe
 import Data.Void
+import Text.Megaparsec hiding (State)
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as Lex
+import Text.Printf
 
 type Parser = Parsec Void String
 
 sourceColI :: SourcePos -> Int
-sourceColI = (subtract 1) . unPos . sourceColumn
+sourceColI = subtract 1 . unPos . sourceColumn
+
+sc :: Parser ()
+sc =
+  Lex.space
+    space1
+    empty
+    empty
+
+lexeme :: Parser a -> Parser a
+lexeme = Lex.lexeme sc
+
+symbol :: String -> Parser String
+symbol = Lex.symbol sc
+
+pInt :: Parser Int
+pInt = lexeme Lex.decimal
+
+pKeyword :: String -> Parser String
+pKeyword keyword = lexeme (string keyword <* notFollowedBy alphaNumChar)
 
 readInt :: String -> Int
 readInt = read
@@ -39,22 +61,22 @@ readInt = read
 splitOnElem :: (Eq a) => a -> [a] -> [[a]]
 splitOnElem e l =
   let g = L.groupBy ((==) `on` (== e)) l
-  in map fst $ filter (even . snd) $ zip g [0..]
+   in map fst $ filter (even . snd) $ zip g [0 ..]
 
 takeWhile2 :: (a -> a -> Bool) -> [a] -> [a]
-takeWhile2 f l = let
-  z = zip (init l) (tail l)
-  matched = L.takeWhile (uncurry f) z
-  in map fst matched
-  
+takeWhile2 f l =
+  let z = zip (init l) (tail l)
+      matched = L.takeWhile (uncurry f) z
+   in map fst matched
+
 readFileLines :: String -> IO [String]
 readFileLines x = lines <$> readFile x
 
 replaceAt :: Int -> (a -> a) -> [a] -> [a]
-replaceAt idx f l = let
-  (a, b1:rb) = splitAt idx l
-  in a ++ f b1:rb
-  
+replaceAt idx f l =
+  let (a, b1 : rb) = splitAt idx l
+   in a ++ f b1 : rb
+
 map2d :: (a -> b) -> [[a]] -> [[b]]
 map2d f = map (map f)
 
@@ -62,7 +84,7 @@ zipWith2d :: (a -> b -> c) -> [[a]] -> [[b]] -> [[c]]
 zipWith2d f = zipWith (zipWith f)
 
 findIndex2d :: (a -> Bool) -> [[a]] -> Maybe (Int, Int)
-findIndex2d f = listToMaybe . catMaybes . zipWith (fmap . flip (,)) [0..] . map (findIndex f)
+findIndex2d f = listToMaybe . catMaybes . zipWith (fmap . flip (,)) [0 ..] . map (findIndex f)
 
 padZeros :: Int -> String
 padZeros = printf "%03d"
@@ -83,7 +105,7 @@ testSample i a f = do
 
 runSample :: Int -> ([String] -> a) -> IO a
 runSample i f = f <$> readFileLines (sampleFile i)
-  
+
 runInput :: Int -> ([String] -> a) -> IO a
 runInput i f = f <$> readFileLines (inputFile i)
 
--- src/a2023/d004/D.hs
+++ src/a2023/d004/D.hs
@@ -8,3 +8,41 @@ import Text.Megaparsec hiding (State)
 import Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as Lex
 
+data Card = Card {cardId::Int, winnings::[Int], picked::[Int]} deriving Show
+
+pLine :: Parser Card
+pLine = do
+  cardId <- pKeyword "Card" *> pInt <* pKeyword ":"
+  winnings <- many pInt
+  _ <- pKeyword "|"
+  picked <- many pInt
+  return Card{..}
+
+pProblem :: Parser [Card]
+pProblem = many pLine
+
+cntCard :: Card -> Int
+cntCard Card{winnings, picked} = let
+  inter = L.intersect winnings picked
+  in if null inter then 0 else 2^(length inter - 1)
+
+type AccumSt = M.Map Int Int
+
+accumCard :: AccumSt -> Card -> AccumSt
+accumCard acc Card{cardId, winnings, picked} = let
+  nWin = length $ L.intersect winnings picked
+  added = M.fromList (zip [cardId+1..cardId+nWin] $ L.repeat (acc M.! cardId))
+  in M.unionWith (+) acc added
+
+sol2 :: String -> Int
+sol2 input = let
+  cards = parseOrError pProblem input
+  initMap = M.fromList (zip [1..length cards] $ L.repeat 1)
+  final = L.foldl' accumCard initMap cards
+  in sum $ M.elems final
+
+sol1 :: String -> Int
+sol1 = sum . map cntCard . parseOrError pProblem
+
+test = sol2 <$> readSample 4
+run = sol2 <$> readInput 4
