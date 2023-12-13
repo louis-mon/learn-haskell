6950e650720932d07d9b757a5f7d1e4904ad3c1d
Louis Monestier <louis.monestier@contractor.akur8.com>
2023-12-13 18:54:25 +0100

    day 13

--- src/a2023/d013/D.hs
+++ src/a2023/d013/D.hs
@@ -1,10 +1,34 @@
 module D where
 
-import Lib
-import qualified Data.Map as M
 import qualified Data.List as L
+import qualified Data.List.Split as Split
+import qualified Data.Map as M
+import Data.Maybe
 import qualified Data.Set as S
+import Lib
 import Text.Megaparsec hiding (State)
 import Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as Lex
 
+isMirror :: Eq a => [a] -> [a] -> Bool
+isMirror x y =
+  let l = min (length x) (length y)
+   in l > 0 && take l x == take l y
+
+isMirrorSmudge :: Eq a => [[a]] -> [[a]] -> Bool
+isMirrorSmudge x y = (== 1) . sum $ zipWith (\lx ly -> length $ filter id $ zipWith (/=) lx ly) x y
+
+findPalindromeIdx :: Eq a => [[a]] -> Maybe Int
+findPalindromeIdx l = L.findIndex (uncurry isMirrorSmudge) $ zip (map reverse $ L.inits l) (L.tails l)
+
+findMirror :: [String] -> Int
+findMirror ls = case findPalindromeIdx ls of
+  Just x -> 100 * x
+  Nothing -> fromJust $ findPalindromeIdx $ L.transpose ls
+
+sol :: [String] -> Int
+sol = sum . map findMirror . splitOnElem ""
+
+test = runSample 13 sol
+
+run = runInput 13 sol
